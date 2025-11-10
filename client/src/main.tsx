fc34e14e82e243cdba463b29f0b5a272d991de1d
Ansam_Almgdlawi <ansamalmgdlawi@gmail.com>
2025-11-10 10:09:22 +0300

    fix error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,3 +3,8 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+document.title = import.meta.env.VITE_APP_TITLE;
+
+const favicon = document.querySelector("link[rel='icon']");
+if (favicon) favicon.setAttribute("href", import.meta.env.VITE_APP_LOGO);
+
